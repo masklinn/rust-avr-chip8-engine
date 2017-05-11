867ad05575ecdfdfbff3e467c047525e6359c273
Dr. ERDI Gergo <gergo@erdi.hu>
2017-05-11 20:37:43 +0800

    #FIXME these noinline pragmas are needed to work around a Rust/AVR bug that I haven't identified yet

--- src/machine.rs
+++ src/machine.rs
@@ -29,6 +29,7 @@ impl Machine {
         }
     }
 
+    #[inline(never)]
     fn arith(op: Arith, x: Byte, y: Byte) -> (Byte, Option<bool>) {
         match op {
             Arith::Load => (y, None),
@@ -56,6 +57,7 @@ impl Machine {
         self.regs[0xf] = if flag { 1 } else { 0 };
     }
 
+    #[inline(never)]
     fn key_coords(key: Byte) -> (Byte, Byte) {
         match key & 0xf {
 
@@ -83,6 +85,7 @@ impl Machine {
         }
     }
 
+    #[inline(never)]
     fn coords_key(coords: (Byte, Byte)) -> Byte {
         match coords {
             (0, 0) => 0x1,
@@ -109,6 +112,7 @@ impl Machine {
         }
     }
 
+    #[inline(never)]
     fn wait_key<P>(&self, io: &P) -> (Byte, Byte) where P: Peripherals {
         let mut init_states = [0; 4];
         for (row, mut state) in init_states.iter_mut().enumerate() {
@@ -137,6 +141,7 @@ impl Machine {
         (0, 0)
     }
 
+    #[inline(never)]
     pub fn step<P>(&mut self, io: &P) where P: Peripherals {
         let hi = io.read_ram(self.pc); self.pc += 1;
         let lo = io.read_ram(self.pc); self.pc += 1;
